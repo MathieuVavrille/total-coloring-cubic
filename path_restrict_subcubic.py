088acaf83ba3584e89107352cb96f697bd7415bd
Mathieu Vavrille <mathieu.vavrille@ens-lyon.fr>
2018-09-20 22:36:17 +0200

    last changes, add of subcubic

--- path_restrict_subcubic.py
+++ path_restrict_subcubic.py
@@ -31,12 +31,12 @@ def find_root(l):
             return 100000000000
     return xn
 
-def get_max_root(A, B, R, list_recurrence):
+def get_max_root(A, B, R, c, list_recurrence):
     """Find the maximum root of a list of recurrences"""
     global results
     r_max = 0
     i_max = 0
-    l = list_recurrence(A, B, R)
+    l = list_recurrence(A, B, R, c)
     all_roots = []
     for i in range(len(l)):
         if False:
@@ -77,7 +77,8 @@ def min_xplus(D, allowed=[(0,0), (0,1), (1,0), (1,1)]):
 def min_to_path(D, P):
     return min(D[0][0]-P[0][0], D[1][0], D[1][1])
 
-def get_list_recurrence(A, B, R):
+def get_list_recurrence(A, B, R, c):
+    #2nd comp = blue
     return ([(4*A[0][0]-2*A[1][0]-B[0][0], )*3+(4*A[0][0]-R[0][0]-2*A[0][1], )*3,            #a00/4*a00
             
             (R[0][0]+min_xplus(A)+min_to_path(A, B), )*2,                                    #r00
@@ -90,7 +91,10 @@ def get_list_recurrence(A, B, R):
             (2*A[0][0]+2*A[1][1]-A[0][1], )*2+(2*A[0][0]+2*A[1][1]-A[1][0], )*2+(2*A[0][0]+2*A[1][1]-R[0][0], )+(2*A[0][0]+2*A[1][1]-B[0][0], ),
             (A[0][0]+3*A[1][1], )*6,                                                         #a00/3*a11
             
-            (2*A[1][1], )*2                                                                 #a11/a11
+            (2*A[1][1], )*2,                                                                 #a11/a11
+            
+            (c+2*min_xplus(A), )+(c+min_xplus(A)+min_yplus(A), )*2+(c+2*min_yplus(A), ),     #degree2
+            (2*c+2*min_xplus(A), )+(2*c+min_xplus(A)+min_yplus(A), )*4+(2*c+2*min_yplus(A), )
             ])
 
 """Remarks
@@ -102,32 +106,36 @@ Cycles colored with the same color are done at the end
 def get_min_coeff_double(N, interval, starting_points):
     """Find the best coefficients such that the max root is the smallest"""
     r_min = 10000
-    c_min = ()
+    c_min = starting_points
     i_min = 0
     l_max = []
     for e1 in range(N+1):
         for e2 in range(N+1):
             for e3 in range(N+1):
-                a00 = 1
-                a10 = e1/(N*interval)+starting_points[0]-1/(2*interval)
-                a11 = e2/(N*interval)+starting_points[1]-1/(2*interval)
-                b00 = e3/(N*interval)+starting_points[2]-1/(2*interval)
-                A = [[0]*3 for j in range(3)]
-                B = [[0]*3 for j in range(3)]
-                R = [[0]*3 for j in range(3)]
-                A[0][0] = a00
-                A[1][0] = a10
-                A[0][1] = a10
-                A[1][1] = a11
-                B[0][0] = b00
-                R[0][0] = b00
-                if True:#0.99 > a01 and a01-0.01 >= a10 >= 0.01 and 0.99 >= b00 and b00-0.01 >= b01 >= 0.01 and 0.99 >= c00 and c00-0.01 >= c01 >= 0.01 and a01-0.01 >= b01:
-                    root, index, l = get_max_root(A, B, R, get_list_recurrence)
-                    if root < r_min:
-                        r_min = root
-                        c_min = (a10, a11, b00)
-                        i_min = index
-                        l_max = l, A
+                for e4 in range(2):
+                    a00 = 1
+                    a10 = e1/(N*interval)+starting_points[0]-1/(2*interval)
+                    a11 = e2/(N*interval)+starting_points[1]-1/(2*interval)
+                    b00 = e3/(N*interval)+starting_points[2]-1/(2*interval)
+                    c   = 1
+                    if c >= 1:
+                        c = 1
+                    A = [[0]*3 for j in range(3)]
+                    B = [[0]*3 for j in range(3)]
+                    R = [[0]*3 for j in range(3)]
+                    A[0][0] = a00
+                    A[1][0] = a10
+                    A[0][1] = a10
+                    A[1][1] = a11
+                    B[0][0] = b00
+                    R[0][0] = b00
+                    if True:#0.99 > a01 and a01-0.01 >= a10 >= 0.01 and 0.99 >= b00 and b00-0.01 >= b01 >= 0.01 and 0.99 >= c00 and c00-0.01 >= c01 >= 0.01 and a01-0.01 >= b01:
+                        root, index, l = get_max_root(A, B, R, c, get_list_recurrence)
+                        if root < r_min:
+                            r_min = root
+                            c_min = (a10, a11, b00, a00)
+                            i_min = index
+                            l_max = l, A
     return r_min, c_min, i_min, l_max
 
 def iterate(nb_steps, update_coeff, N, interval, starting):
@@ -148,7 +156,7 @@ def iterate(nb_steps, update_coeff, N, interval, starting):
     #print(l_max[1])
     print(log(r_min1, 2))
 
-iterate(12, 2, 7, 1, [0.5, 0.5, 0.5])
+iterate(20, 2, 5, 1, [0.5, 0.5, 0.5, 0.5])
 
 #scp Bureau/stage/total-coloring-cubic/double_color.py  vavrille@chuck.mimuw.edu.pl:~/            (with -R for folder)
 
